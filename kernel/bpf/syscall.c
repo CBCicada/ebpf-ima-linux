9ccc60c9bb63afb661543a3ee3a69170f12155c1
CBCicada <s@cbcicada.com>
2025-11-25 22:05:58 -0600

    added ifdef

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -3083,9 +3083,11 @@ static int bpf_prog_load(union bpf_attr *attr, bpfptr_t uattr, u32 uattr_size)
 	 * Here eBPF program has been verified and is ready 
 	 * for attached, check if policy allows this
 	 */
+	#ifdef CONFIG_IMA
 	err = ima_bpf_prog_load(prog, attr->prog_name, attr, uattr, uattr_size);
 	if (err < 0)
 		goto free_used_maps;
+	#endif
 	
 	prog = bpf_prog_select_runtime(prog, &err);
 	if (err < 0)
